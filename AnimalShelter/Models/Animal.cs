aae33ad1a4745224f44f8a97cd60a905c3dce2b7
Jacob Maier <jacobamaier@gmail.com>
2023-03-07 11:38:02 -0800

    new build attempt.
    Co-authored-by: David Jandron  <jandron1211@gmail.com>
    Co-Authored by: Mesha Devan <madsketchz@gmail.com>
    Co-authored-by : Erin Timlin
    <erintimlin@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -5,7 +5,7 @@ namespace AnimalShelter.Models
     public int AnimalId { get; set; }
     public string Description { get; set; }
     public string Name { get; set; }
-    public string Arrival { get; set; } 
+    public string ArrivalDate { get; set; } 
 
   }
 }
