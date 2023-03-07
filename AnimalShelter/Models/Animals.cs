908d1876bb8384ff16a1599e13058d830a76eb3e
Jacob Maier <jacobamaier@gmail.com>
2023-03-07 10:22:35 -0800

    Connect database.
    Co-authored-by: David Jandron  <jandron1211@gmail.com>
    Co-authored-by : Erin Timlin
    <erintimlin@gmail.com>
    Co-Authored by: Mesha Devan <madsketchz@gmail.com>

--- AnimalShelter/Models/Animals.cs
+++ AnimalShelter/Models/Animals.cs
@@ -4,6 +4,8 @@ namespace AnimalShelter.Models
   {
     public int AnimalId { get; set; }
     public string Description { get; set; }
+    public string Name { get; set; }
+    public string Arrival { get; set; } 
 
   }
 }
